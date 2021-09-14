152d9495e7fff74ff16ec0ef552112219f4276ef
KentaroJay <kentaro.jay.takahashi@gmail.com>
2021-09-14 20:17:02 +0900

    minor changes

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ const app = express();
 
 app.set("trust proxy", 1);
 
+app.use(session({
+  secret: 'reply-analyzer',
+  name: "spa",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    sameSite: 'none',
+    secure: true
+  }
+}));
+
+app.use(cors());
+
 
 //静的ファイルの読み込み
 app.use(
@@ -14,25 +27,6 @@ app.use(
   express.static(path.resolve(__dirname, "frontend", "static"))
 );
 
-app.use(session({
-  allowedHeaders: ['sessionId', 'Content-Type'],
-  exposedHeaders: ['sessionId'],
-  secret: 'reply-analyzer',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    secure: true,
-    sameSite: 'none'
-  }
-}));
-
-app.use(
-  cors({
-    origin: "https://beat-chain.herokuapp.com",
-    credentials: true,
-    optionsSuccessStatus: 200
-  })
-);
 
 //リクエスト処理
 app.get("/*", (req, res) => {
