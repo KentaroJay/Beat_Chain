86235023d54148b5a96f921100709448d41625fb
KentaroJay <kentaro.jay.takahashi@gmail.com>
2021-09-15 01:41:42 +0900

    minor changes on server.js and packages

--- server.js
+++ server.js
@@ -1,22 +1,36 @@
 const express = require("express");
 const session = require('express-session');
 const cors = require("cors");
+var cookieParser = require('cookie-parser');
 const path = require("path");
 
 const app = express();
 app.set("trust proxy", 1);
+app.use(cookieParser());
 
 app.use(
   session({
     secret: process.env.SESSION_SECRET || 'Super Secret (change it)',
     resave: true,
+    domain: 'http://localhost:8000',
     saveUninitialized: false,
     cookie: {
+      path: "/",
+      domain: 'http://localhost:8000',
       sameSite: 'none', // must be 'none' to enable cross-site delivery
       secure: true, // must be true if sameSite='none'
+      maxAge: 24 * 60 * 1000
     }
   })
 );
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  next();
+});
 
 app.use(
   cors({
