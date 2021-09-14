336b06d3bbf2cee9a240f7b3b328bac26279af93
KentaroJay <kentaro.jay.takahashi@gmail.com>
2021-09-14 17:28:00 +0900

    minor changes

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const expressSession = require('express-session');
+const cors = require("cors");
 const path = require("path");
 
 const app = express();
@@ -10,6 +12,22 @@ app.set("trust proxy", 1);
 app.use(
   "/static",
   express.static(path.resolve(__dirname, "frontend", "static"))
+);
+
+app.use(
+  expressSession({
+    secret: process.env.SESSION_SECRET || 'Super Secret (change it)',
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+      sameSite: process.env.NODE_ENV === "production" ? 'none' : 'lax', // must be 'none' to enable cross-site delivery
+      secure: process.env.NODE_ENV === "production", // must be true if sameSite='none'
+    }
+  })
+);
+
+app.use(
+  cors()
 );
 
 //リクエスト処理
